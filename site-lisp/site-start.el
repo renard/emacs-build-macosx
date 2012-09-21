b6ff21817f3a2b52d2e6922055cd17972b34aa14
Sébastien Gross <sebchezwamorg>
2012-09-21 10:01:35 +0200

    Add site-start customization script.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ site-lisp/site-start.el
@@ -0,0 +1,25 @@
+;;; site-start.el --- 
+
+;; Copyright © 2012 Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+
+;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+;; Keywords: emacs, 
+;; Created: 2012-09-21
+;; Last changed: 2012-09-21 09:29:40
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;; 
+
+
+;;; Code:
+
+(setq source-directory
+      (expand-file-name  "../src" data-directory))
+
+
+(provide 'site-start)
+
+;; site-start.el ends here
