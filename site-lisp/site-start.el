0e7cc0b8e26ffa6e8ef50db461430addf3c30962
Sébastien Gross <sebchezwamorg>
2012-09-28 23:44:13 +0200

    Add some useful functions about how Emacs was patched.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- site-lisp/site-start.el
+++ site-lisp/site-start.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-09-21
-;; Last changed: 2012-09-28 23:00:13
+;; Last changed: 2012-09-28 23:43:28
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -15,6 +15,11 @@
 
 
 ;;; Code:
+
+(eval-when-compile
+  (require 'dired)
+  (require 'files)
+  (require 'subr))
 
 (setq source-directory
       (expand-file-name  ".." data-directory))
@@ -31,7 +36,36 @@ See both `http://emacswiki.org/emacs/EmacsFromGit' and
 `http://savannah.gnu.org/projects/emacs' for further
 information.")
 
+(defvar emacs-patches-directory
+  (expand-file-name  "../patches" data-directory)
+  "Directory containing all patches used when Emacs was built.")
 
+(defvar emacs-patches-list
+  (directory-files emacs-patches-directory nil ".*\\.patch" nil)
+  "List of all patches applied when Emacs was built.")
+
+(defun view-emacs-patches-dir ()
+  "Open `dired' in `emacs-patches-directory'."
+  (interactive)
+  (dired emacs-patches-directory))
+
+(defun view-emacs-patches ()
+  "Open `dired' in `emacs-patches-directory'."
+  (interactive)
+  (dired emacs-patches-directory))
+
+(defun view-emacs-patch (&optional patch)
+  "Find PATCH used when Emacs was built in
+`emacs-patches-directory'."
+  (interactive)
+  (let* ((patch (or patch
+		    (completing-read "View patch: "
+				     emacs-patches-list nil t)))
+	 (patch-file (format "%s/%s" emacs-patches-directory patch)))
+    (if (file-exists-p patch-file)
+	(find-file patch-file)
+      (error "No patch matching %s found in %s."
+	     patch emacs-patches-directory))))
 
 (provide 'site-start)
 
