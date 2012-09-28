0fd857979244b4c55ad1546dc279ba599a5371b8
Sébastien Gross <sebchezwamorg>
2012-09-28 23:02:20 +0200

    Add emacs-version-git-commit support.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- site-lisp/site-start.el
+++ site-lisp/site-start.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-09-21
-;; Last changed: 2012-09-21 09:29:40
+;; Last changed: 2012-09-28 23:00:13
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -18,6 +18,19 @@
 
 (setq source-directory
       (expand-file-name  ".." data-directory))
+
+(defconst emacs-version-git-commit "@@GIT_COMMIT@@"
+  "String giving the git sha1 from which this Emacs was built.
+
+SHA1 of git commit that was used to compile this version of
+emacs. The source used to compile emacs are taken from Savannah's
+git repository at `http://git.savannah.gnu.org/r/emacs.git' or
+git://git.savannah.gnu.org/emacs.git
+
+See both `http://emacswiki.org/emacs/EmacsFromGit' and
+`http://savannah.gnu.org/projects/emacs' for further
+information.")
+
 
 
 (provide 'site-start)
