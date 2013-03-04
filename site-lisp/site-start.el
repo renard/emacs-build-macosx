a3b6b78d75ab80bfcdd975866348c2587d6ac79f
Sébastien Gross <sebchezwamorg>
2013-03-04 15:21:06 +0100

    Allow to search user configuration from an other directory than ~/.emacs.d/.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- site-lisp/site-start.el
+++ site-lisp/site-start.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-09-21
-;; Last changed: 2012-09-28 23:56:56
+;; Last changed: 2013-03-03 20:10:56
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -17,6 +17,7 @@
 ;;; Code:
 
 (eval-when-compile
+  (require 'cl)
   (require 'dired)
   (require 'files))
 
@@ -42,6 +43,22 @@ information.")
 (defvar emacs-patches-list
   (directory-files emacs-patches-directory nil ".*\\.patch" nil)
   "List of all patches applied when Emacs was built.")
+
+(defconst user-emacs-directory
+  (loop for d in
+	;; try to load emacs init file from several version based
+	;; directories. Use default directory if not found.
+	(list (format "~/.emacs.d-%s/" emacs-version-git-commit)
+	      (format "~/.emacs.d-%s/" emacs-version)
+	      (format "~/.emacs.d-%d/" emacs-major-version)
+	      (format "~/.emacs.d-%d.%d/" emacs-major-version emacs-minor-version)
+	      "~/.emacs.d/")
+	when (and (file-directory-p d)
+		  (file-exists-p (concat d "init.el")))
+	do (setq user-init-file
+		 (expand-file-name (concat d "init.el")))
+	and return d))
+(message "Using %s" user-init-file)
 
 (defun view-emacs-patches ()
   "Open `dired' in `emacs-patches-directory'."
